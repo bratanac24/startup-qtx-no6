9648b05eed8a5b4ae69e1d431b629ad12234924c
bratanac24 <acar998@gmail.com>
2021-10-20 18:38:32 +0200

    react bug fix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>VERCEL TEST</code> and save to reload.
+          Edit <code>REACT TEST</code> and save to reload.
         </p>
         <a
           className="App-link"
